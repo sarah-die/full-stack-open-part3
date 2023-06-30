d2644f3b8ffc04771deb224339dde6d815cc918b
sarah-die <saridi@alice.de>
2023-06-30 12:45:28 +0200

    3.7: Phonebook backend step 7

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 app.use(express.json());
+app.use(morgan("tiny"));
 
 let persons = [
   {
@@ -66,12 +68,9 @@ app.delete("/api/persons/:id", (req, res) => {
 
 // 3.5 + 3.6: Phonebook backend, step 5 + step 6
 const generateRandomId = () => {
-  const length = 8;
   let newId = 0;
   while (newId === 0) {
-    newId = Math.random()
-      .toString(36)
-      .substring(2, length + 2);
+    newId = Math.floor(Math.random()*1000)
     if (persons.find((p) => p.id === newId)) {
       newId = 0;
     }
