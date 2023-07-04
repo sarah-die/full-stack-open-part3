aeab22d84db54a5ff775cb92d2670d15a51f1660
sarah-die <saridi@alice.de>
2023-07-04 16:24:37 +0200

    3.13: Phonebook database, step 1 (fetching data from mongoDB)

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
 const cors = require("cors");
+const morgan = require("morgan");
+require("dotenv").config();
+
+// imp! import dotenv before person model
+const Person = require("./models/person");
 
 app.use(express.json());
 app.use(cors());
@@ -20,57 +24,50 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/", (req, res) => {
   res.send("<h1>API for person data</h1>");
 });
 
 // 3.1: Phonebook backend, step 1
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
+// app.get("/api/persons", (req, res) => {
+//   res.json(persons);
+// });
+
+// 3.13 Phonebook database, step 1
+app.get("/api/persons/", (request, response) => {
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
-// 3.2: Phonebook backend, step 2
+// 3.2: Phonebook backend, step 2 / 3.13 Phonebook database, step 1
 app.get("/info", (req, res) => {
-  res.send(
-    `<div>Phonebook has info for ${
-      persons.length
-    } people</div><br/><div>${new Date().toString()}</div>`
-  );
+  Person.find({}).then((persons) => {
+    res.send(
+      `<div>Phonebook has info for ${
+        persons.length
+      } people</div><br/><div>${new Date().toString()}</div>`
+    );
+  });
 });
 
 // 3.3: Phonebook backend, step 3
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
+// app.get("/api/persons/:id", (req, res) => {
+//   const id = Number(req.params.id);
+//   const person = persons.find((person) => person.id === id);
+//
+//   if (person) {
+//     res.json(person);
+//   } else {
+//     res.status(404).end();
+//   }
+// });
 
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+// 3.13 Phonebook database, step 1
+app.get("/api/persons/:id", (request, response) => {
+  Person.findById(request.params.id).then((person) => {
+    response.json(person);
+  });
 });
 
 // 3.4: Phonebook backend, step 4
@@ -123,7 +120,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
